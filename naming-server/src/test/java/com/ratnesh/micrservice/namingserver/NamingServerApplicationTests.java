b1eda7a048343eb0e710471d509ab56dce01b077
RatneshPorwal <ratneshporwal@gmail.com>
2022-07-28 12:42:22 +0530

    Basic Eureka Server

---
+++ naming-server/src/test/java/com/ratnesh/micrservice/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratnesh.micrservice.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
